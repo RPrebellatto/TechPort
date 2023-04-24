4764a5b6c54be0c00cb7eeccbb9e44bf284f9b04
RPRebellatto <rogerio.rebellatto@gmail.com>
2023-04-24 16:18:57 -0300

    Nova conta de database adicionada na exclusivamente na branch paginação. Migration feita para atualizar banco. Pendente de preenchimento de campos

--- Migrations/20230424191441_NovaConta.Designer.cs
+++ Migrations/20230424191441_NovaConta.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechPort.Data;
 
@@ -11,9 +12,10 @@ using TechPort.Data;
 namespace TechPort.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230424191441_NovaConta")]
+    partial class NovaConta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230424191441_NovaConta.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechPort.Migrations
+{
+    public partial class NovaConta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
